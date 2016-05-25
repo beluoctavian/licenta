e06263ee33fe675835c83ee802acc71bbb8e7b6d
Belu Octavian <belu.octavian@yahoo.com>
2016-05-25 12:28:05 +0300

    First version of working Google Crawler

--- docroot/app/Http/Controllers/GoogleCrawler.php
+++ docroot/app/Http/Controllers/GoogleCrawler.php
@@ -16,7 +16,7 @@ class GoogleCrawler extends Controller
    * GoogleCrawler constructor.
    * @param string $url
    */
-  public function __construct($url = 'https://www.google.com/search') {
+  public function __construct($url = 'https://www.google.com') {
     $this->url = $url;
   }
 
@@ -24,14 +24,26 @@ class GoogleCrawler extends Controller
    * Returns Google search results for a specific query.
    *
    * @param string $query
+   *  The searched text.
+   * @param int $limit
+   *  The number of results returned.
+   * @param bool $advanced
+   *  If set to TRUE, the crawler will create a new request for every result to
+   * get the full page content.
    * @return array
+   *  An array of arrays containing:
+   *    - url: Url to results webpage
+   *    - title: Title of webpage
+   *    - summary: Summary of webpage content
+   *    - content: Parsed content of webpage if the $advanced parameter is set to
+   * TRUE, NULL otherwise
    * @throws \Exception
    */
-  public function search($query) {
+  public function search($query, $limit = 10, $advanced = FALSE) {
     if (!is_string($query)) {
       throw new \Exception('The query string is not valid.');
     }
-    $url = $this->url . '?q=' . urlencode($query);
+    $url = $this->url . '/search?q=' . urlencode($query);
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HEADER, TRUE);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
@@ -56,8 +68,25 @@ class GoogleCrawler extends Controller
       if ($div->getAttribute('class') != 'g') {
         continue;
       }
+      $item = [];
+      /** @var \DOMElement $h3 */
       $h3 = $div->getElementsByTagName('h3')->item(0);
+      $a = $h3->firstChild;
+      $item['url'] = $item['title'] = $item['summary'] = $item['content'] = NULL;
+      $item['url'] = $this->url . $a->getAttribute('href');
+      $item['title'] = strip_tags($a->textContent);
+      foreach ($div->getElementsByTagName('span') as $span) {
+        /** @var \DOMElement $span */
+        if ($span->getAttribute('class') != 'st') {
+          continue;
+        }
+        $item['summary'] = strip_tags($span->textContent);
+        break;
+      }
+      if (!empty($item['url'])) {
+        $items[] = $item;
+      }
     }
-    return [];
+    return $items;
   }
 }
