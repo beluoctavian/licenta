1802acb21ae5dfa23b831011a78a1ca2fd9298ee
Belu Octavian <belu.octavian@yahoo.com>
2016-05-25 12:59:47 +0300

    Render the search results

--- docroot/resources/views/search/home.blade.php
+++ docroot/resources/views/search/home.blade.php
@@ -12,11 +12,20 @@
             @endif
             <div class="content">
                 <form id="search-form" method="get" action="/search">
-                    <input name="q" type="text" maxlength="2048" class="search-text">
+                    <input name="q" type="text" maxlength="2048" class="search-text" value="{{ !empty($_GET['q']) ? $_GET['q'] : '' }}">
                     <button type="submit" class="search-form-submit">
                         <span class="fa fa-search" aria-hidden="true"></span>
                     </button>
                 </form>
+                @if (!empty($results))
+                    <div class="results">
+                        @foreach($results as $result)
+                            <div class="item">
+                                <h3><a href="{{ $result['url'] }}">{{ $result['title'] }}</a></h3>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
             </div>
         </div>
     </body>
